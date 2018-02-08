f8329c1eef797b2c7cab198140ed4c18bc48570d
BuildTools <33381901+JustinLex@users.noreply.github.com>
2018-02-08 18:13:08 +0100

    Finished exercise 1

--- mipslabwork.c
+++ mipslabwork.c
@@ -6,7 +6,7 @@
    This file should be changed by YOU! So you must
    add comment(s) here with your name(s) and date(s):
 
-   This file modified 2017-04-31 by Ture Teknolog 
+   This file modified 2017-04-31 by Ture Teknolog
 
    For copyright and licensing, see file COPYING */
 
@@ -14,7 +14,7 @@
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
 
-int mytime = 0x5957;
+int mytime = 0x0000;
 
 char textstring[] = "text, more text, and even more text!";
 
@@ -27,16 +27,31 @@ void user_isr( void )
 /* Lab-specific initialization goes here */
 void labinit( void )
 {
+  volatile uint8_t* reg_trise_clr = 0xbf886104; //output-enable register
+  *reg_trise_clr = 0xff;
+  volatile uint32_t* reg_trisd_set = 0xbf8860c8; //input-enable register
+  *reg_trisd_set = 0xfd0;
   return;
 }
 
 /* This function is called repetitively from the main program */
 void labwork( void )
 {
-  delay( 1000 );
+  volatile uint8_t* reg_porte = 0xbf886110; //IO port E output register
+  delay( 10 );
   time2string( textstring, mytime );
   display_string( 3, textstring );
   display_update();
   tick( &mytime );
+  if(getbtns)
+  {
+      if((getbtns&0x4))
+      mytime=(mytime&0xf0ff)|(getsw<<8);
+      if((getbtns&0x2))
+      mytime=(mytime&0xf0ff)|(getsw<<4);
+      if((getbtns&0x1))
+      mytime=(mytime&0xff0f)|(getsw);
+  }
+  (*reg_porte)++; //set the leds to show the current time in binary
   display_image(96, icon);
 }
